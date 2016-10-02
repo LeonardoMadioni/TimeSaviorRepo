05be40130f6ec60707e67280687d5769dbf6d102
EmanueleMoi96 <moi.emanuele@gmail.com>
2016-10-02 19:10:20 +0200

    First Commit
    Daje

---
+++ TheTimeSavior/Assets/Scripts/destroyer_player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class destroyer_player : MonoBehaviour
+{
+    
+    public  float velocityTimeProblem = 2f;
+    
+    void Start()
+    {
+
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * velocityTimeProblem;
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player") SceneManager.LoadScene("Level");
+
+    }
+
+  
+}
